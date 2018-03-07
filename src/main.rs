7b8abdf4337e889b728121e3b87c71f58f660186
Matthew Fisher <matt.fisher@microsoft.com>
2018-03-06 22:44:33 -0800

    add logging

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 extern crate clap;
+#[macro_use]
+extern crate log;
+extern crate simple_logger;
+
 use clap::{Arg, App};
+use log::Level;
 
 fn main() {
+    let log_level;
+
     let matches = App::new("helm-lintr")
                           .version("0.1.0")
                           .author("Matthew Fisher <matt.fisher@microsoft.com>")
@@ -23,18 +30,21 @@ fn main() {
                           .get_matches();
 
     // Vary the output based on how many times the user used the "verbose" flag
-    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
+    // (i.e. 'helm-lintr -v -v -v' or 'helm-lintr -vvv'
     match matches.occurrences_of("v") {
-        0 => println!("No verbose info"),
-        1 => println!("Some verbose info"),
-        2 => println!("Tons of verbose info"),
-        3 | _ => println!("Don't be crazy"),
+        0 => log_level = Level::Error,
+        1 => log_level = Level::Warn,
+        2 => log_level = Level::Info,
+        3 => log_level = Level::Debug,
+        4 | _ => log_level = Level::Trace,
     }
+
+    simple_logger::init_with_level(log_level).unwrap();
 
     // Gets a value for config if supplied by user, or defaults to "lintr.conf"
     let config = matches.value_of("config").unwrap_or("lintr.conf");
-    println!("Value for config: {}", config);
+    debug!("Value for config: {}", config);
 
     // Calling .unwrap() is safe here because "PATH" is required
-    println!("Using chart file: {}", matches.value_of("PATH").unwrap());
+    debug!("Using chart file: {}", matches.value_of("PATH").unwrap());
 }
